9161561338b6a0a9283db338cef1c49d452cf502
Maciek Mika <maciekmika@hotmail.com>
2023-01-09 20:30:22 +0100

    exercise chapter 4 added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use crate::spin_lock::SpinLock;
+use std::thread;
+
+mod spin_lock;
+
 fn main() {
-    println!("Hello, world!");
+    let x = SpinLock::new(Vec::new());
+    thread::scope(|s| {
+        s.spawn(|| x.lock().push(1));
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
+    });
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 3] || g.as_slice() == [2, 2, 1])
 }
